bf24463edc4a13b36f691c5082b5f64447cb4e2f
rasimumut <rsmumtozkrt5@gmail.com>
2021-03-14 21:11:57 +0300

    14.03.21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { ChartsModule } from 'ng2-charts';
+import { MyPieChartComponent } from './my-pie-chart/my-pie-chart.component';
+import { RequestsComponent } from './requests/requests.component';
+import { MyLineChartComponent } from './my-line-chart/my-line-chart.component';
+import { InfonavComponent } from './infonav/infonav.component';
+import { AdminComponent } from './admin/admin.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyPieChartComponent,
+    RequestsComponent,
+    MyLineChartComponent,
+    InfonavComponent,
+    AdminComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
